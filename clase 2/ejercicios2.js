f6387c8a9bf9f22c96725cfce45723c6b290a34b
Joao <joao@appsimplantadores.com>
2024-10-29 16:51:17 +0100

    clase2

---
+++ clase 2/ejercicios2.js
@@ -0,0 +1,18 @@
+ const numeros = new Set();
+numeros.add(1);
+numeros.add(2);
+numeros.add(3);
+numeros.add(4);
+
+
+
+let temTres = numeros.has(2)
+
+numeros.delete(2)
+
+
+
+console.log(numeros)
+console.log(temTres)
+
+
